9beea35bb085566dd06365c13e4f4e44cb93baab
TJ Taylor <dugancathal@gmail.com>
2014-09-05 21:00:14 -0600

    Wrap the PeerConnection methods as promises

--- spec/javascripts/peer_connection_spec.js
+++ spec/javascripts/peer_connection_spec.js
@@ -30,7 +30,7 @@ describe('PeerConnection', function () {
       var addStreamSpy = jasmine.createSpy('addStream');
 
       $scope.$on('rtc.addstream', addStreamSpy);
-      pc.onaddstream({stream: 1});
+      pc.connection.onaddstream({stream: 1});
 
       expect(addStreamSpy.calls.mostRecent().args[0].name).toEqual('rtc.addstream');
       expect(addStreamSpy.calls.mostRecent().args[1]).toEqual({stream: 1});
@@ -42,7 +42,7 @@ describe('PeerConnection', function () {
     it('ignores empty candidates', function () {
       var pc = new PeerConnection({config: 'data'}, 14);
       spyOn(Signaler, 'sendToPeer');
-      pc.onicecandidate({});
+      pc.connection.onicecandidate({});
       
       expect(Signaler.sendToPeer).not.toHaveBeenCalled();
     });
@@ -50,7 +50,7 @@ describe('PeerConnection', function () {
     it('sends valid candidates to its peer', function () {
       var pc = new PeerConnection({config: 'data'}, 14);
       spyOn(Signaler, 'sendToPeer');
-      pc.onicecandidate({candidate: {sdpMLineIndex: 4, sdpMid: 3, candidate: 'some long sdp'}});
+      pc.connection.onicecandidate({candidate: {sdpMLineIndex: 4, sdpMid: 3, candidate: 'some long sdp'}});
       
       expect(Signaler.sendToPeer).toHaveBeenCalledWith(14, {
         type: 'IceCandidateMessage',
@@ -58,6 +58,124 @@ describe('PeerConnection', function () {
         label: 4,
         candidate: 'some long sdp'
       });
+    });
+  });
+
+  describe('createOffer', function () {
+    it('creates a promise for connection.createOffer', function () {
+      var onSuccess, onError;
+      fakeRtcPeerConnection.createOffer = function createOffer(success, error) {
+        onSuccess = success;
+        onError = error;
+      };
+
+      var pc = new PeerConnection({}, 12);
+      var successFn = jasmine.createSpy('successCallback'), errorFn = jasmine.createSpy('errorCallback');
+
+      pc.createOffer().then(successFn, null);
+
+      onSuccess();
+      $scope.$digest();
+      expect(successFn).toHaveBeenCalled();
+
+      pc.createOffer().then(null, errorFn);
+
+      onError();
+      $scope.$digest();
+      expect(errorFn).toHaveBeenCalled();
+    });
+  });
+
+  describe('createAnswer', function () {
+    it('creates a promise for connection.createAnswer', function () {
+      var onSuccess, onError;
+      fakeRtcPeerConnection.createAnswer = function createAnswer(success, error) {
+        onSuccess = success;
+        onError = error;
+      };
+
+      var pc = new PeerConnection({}, 12);
+      var successFn = jasmine.createSpy('successCallback'), errorFn = jasmine.createSpy('errorCallback');
+
+      pc.createAnswer().then(successFn, null);
+
+      onSuccess();
+      $scope.$digest();
+      expect(successFn).toHaveBeenCalled();
+
+      pc.createAnswer().then(null, errorFn);
+
+      onError();
+      $scope.$digest();
+      expect(errorFn).toHaveBeenCalled();
+    });
+  });
+
+  describe('setLocalDescription', function () {
+    it('creates a promise for connection.setLocalDescription', function () {
+      var description, onSuccess, onError;
+      fakeRtcPeerConnection.setLocalDescription = function setLocalDescription(descr, success, error) {
+        description = descr;
+        onSuccess = success;
+        onError = error;
+      };
+
+      var pc = new PeerConnection({}, 12);
+      var successFn = jasmine.createSpy('successCallback'), errorFn = jasmine.createSpy('errorCallback');
+
+      pc.setLocalDescription('description').then(successFn, null);
+
+      expect(description).toEqual('description');
+
+      onSuccess();
+      $scope.$digest();
+      expect(successFn).toHaveBeenCalled();
+
+      pc.setLocalDescription().then(null, errorFn);
+
+      onError();
+      $scope.$digest();
+      expect(errorFn).toHaveBeenCalled();
+    });
+  });
+
+  describe('setRemoteDescription', function () {
+    it('creates a promise for connection.setRemoteDescription', function () {
+      var description, onSuccess, onError;
+      fakeRtcPeerConnection.setRemoteDescription = function setRemoteDescription(descr, success, error) {
+        description = descr;
+        onSuccess = success;
+        onError = error;
+      };
+
+      var pc = new PeerConnection({}, 12);
+      var successFn = jasmine.createSpy('successCallback'), errorFn = jasmine.createSpy('errorCallback');
+
+      pc.setRemoteDescription('description').then(successFn, null);
+
+      expect(description).toEqual('description');
+
+      onSuccess();
+      $scope.$digest();
+      expect(successFn).toHaveBeenCalled();
+
+      pc.setRemoteDescription().then(null, errorFn);
+
+      onError();
+      $scope.$digest();
+      expect(errorFn).toHaveBeenCalled();
+    });
+  });
+
+  describe('addIceCandidate', function () {
+    it('adds an ICE candidate to the connection', function () {
+      fakeRtcPeerConnection.addIceCandidate = function () {};
+      spyOn(fakeRtcPeerConnection, 'addIceCandidate');
+
+      var pc = new PeerConnection({}, 12);
+      pc.addIceCandidate({candidate: 'sdp'});
+
+      expect(fakeRtcPeerConnection.addIceCandidate).toHaveBeenCalledWith({candidate: "sdp"});
     });
   });
 });
