27c9e6f77c855a6269821d5032984748bdc56c52
TJ Taylor <dugancathal@gmail.com>
2014-09-07 13:37:08 -0600

    Add some styling and an Form flow

--- lib/video_conference/app.rb
+++ lib/video_conference/app.rb
@@ -3,7 +3,20 @@ require 'sinatra/base'
 module VideoConference
   class App < Sinatra::Base
     get '/' do
-      haml :index
+      redirect to('/vids/new')
+    end
+
+    get '/vids/new' do
+      haml :new
+    end
+
+    post '/vids' do
+      redirect to("/vids/#{params['name']}")
+    end
+
+    get '/vids/:name' do
+      @name = params[:name]
+      haml :show
     end
   end
 end
